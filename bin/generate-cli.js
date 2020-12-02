0f3989cf1094f72e4ee6ecbd26029370b8090a49
JL_Guan <JL_Guan@yeah.net>
2020-12-02 10:46:47 +0800

    init generate-cli

---
+++ bin/generate-cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const Creator = require('../lib/creator')
+const { projectName, version, typeNames, chalk } = require('../lib/share-util')
+
+// 提示
+program
+    .name(projectName)
+    .action(() => {
+        new Creator();
+    })
+
+program
+    .command('create [typeName]')
+    .description('create a new [typeName] project')
+    .action(typeName => {
+        if (!typeNames.includes(typeName)) {
+            console.log()
+            console.log(
+                `  ` + chalk.red(`Unknown typeName ${chalk.yellow(typeName)}.`)
+            )
+            console.log()
+            return
+        }
+        console.log()
+        console.log()
+        console.log(chalk.blue(`${projectName} v${version}`))
+        new Creator(typeName)
+    })
+
+program.commands.forEach(c => c.on('--help', () => console.log()))
+
+program.parse(process.argv)
---
+++ template/vue/vue.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: JL Guan
+ * @Date: 2020-12-02 10:34:47
+ * @description: file description
+ * @LastEditTime: 2020-12-02 10:37:21
+ * @FilePath: \mine\generate-cli\template\vue\vue.config.js
+ */
+module.exports = {
+    publicPath: './',
+    lintOnSave: false,
+}
