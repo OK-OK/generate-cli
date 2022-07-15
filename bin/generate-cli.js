7d9e82c9e2ac58d6fd65f4788b5b5fe45c4e93f5
JL_Guan <JL_Guan@yeah.net>
2022-07-15 15:43:47 +0800

    modify: 优化逻辑实现

--- bin/generate-cli.js
+++ bin/generate-cli.js
@@ -1,34 +1,32 @@
 #!/usr/bin/env node
 
-const program = require('commander')
-const Creator = require('../lib/creator')
-const { projectName, version, commands, chalk } = require('../lib/share-util')
+const program = require("commander")
+const Creator = require("../lib/creator")
+const { projectName, version, commands, chalk } = require("../lib/share-util")
 
 // 提示
-program
-    .name(projectName)
-    .action(() => {
-        new Creator();
-    })
+program.name(projectName).action(() => {
+  new Creator()
+})
 
 program
-    .command('create [typeName]')
-    .description('create a new [typeName] project')
-    .action(typeName => {
-        if (![...commands.keys()].includes(typeName)) {
-            console.log()
-            console.log(
-                `  ` + chalk.red(`Unknown typeName ${chalk.yellow(typeName)}.`)
-            )
-            console.log()
-            return
-        }
-        console.log()
-        console.log()
-        console.log(chalk.blue(`${projectName} v${version}`))
-        new Creator(typeName)
-    })
+  .command("create [typeName]")
+  .description("create a new [typeName] project")
+  .action(typeName => {
+    if (![...commands.keys()].includes(typeName)) {
+      console.log()
+      console.log(
+        `  ` + chalk.red(`Unknown typeName ${chalk.yellow(typeName)}.`)
+      )
+      console.log()
+      return
+    }
+    console.log()
+    console.log()
+    console.log(chalk.blue(`${projectName} v${version}`))
+    new Creator(typeName)
+  })
 
-program.commands.forEach(c => c.on('--help', () => console.log()))
+program.commands.forEach(c => c.on("--help", () => console.log()))
 
 program.parse(process.argv)
